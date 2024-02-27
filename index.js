986456e17cc612eed2161b9fb9ab7bb4a6267398
z31430158@gmail.com <zarmanimran@gmail.com>
2024-02-27 07:30:15 +0500

    changed amount of digits

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const log = console.log;
 program.version('1.0.0').description('Password Generator');
 
 program
-  .option('-l, --length <number>', 'Length of password', '10')
+  .option('-l, --length <number>', 'Length of password', '15')
   .option('-s , --save', 'save password to passwords.txt')
   .option('-nn , --no-numbers', 'remove numbers')
   .option('-ns , --no-symbols', 'remove symbols')
